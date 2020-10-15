0fdb400b045e06c3340fcb421a9e49069606680c
aspa <kreoll@gmail.com>
2020-10-15 21:35:52 +0300

    1.7. Состояние объекта [#387589] https://job4j.ru/edu/task?action=task&taskId=0873958f751ce70201751ceec4ca049f&number=387589

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery1 = new Battery(30);
+        Battery battery2 = new Battery(50);
+        System.out.println("battery1: " + battery1.load + "; battery2: " + battery2.load);
+        battery1.exchange(battery2);
+        System.out.println("battery1: " + battery1.load + "; battery2: " + battery2.load);
+
+    }
+}
