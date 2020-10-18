97e209b2f0cc6185a8550ae4ba6d7044cbb5cba9
aspa <kreoll@gmail.com>
2020-10-18 18:49:50 +0300

    7. toString [#387600] https://job4j.ru/edu/task?action=task&taskId=0873958f751ce70201751ceec58304aa&number=387600

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -57,4 +57,9 @@ public class Item {
         System.out.println("---");
 
     }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\'' + ", created=" + created + '}';
+    }
 }
