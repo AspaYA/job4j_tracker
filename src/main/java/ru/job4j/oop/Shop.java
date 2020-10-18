bcf6753221c40f7f660f3bcb436f4329696baec0
aspa <kreoll@gmail.com>
2020-10-18 18:31:16 +0300

    6. Почему наследование - это плохо [#387599] https://job4j.ru/edu/task?action=task&taskId=0873958f751ce70201751ceec57204a9&number=387599

---
+++ src/main/java/ru/job4j/oop/Shop.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product product = of("Oil", 100);
+        System.out.println(product.label());
+    }
+
+    public static Product of(String name, int price) {
+        if ("Oil".equals(name)) {
+            System.out.println("gen LiquidationProduct");
+            LiquidationProduct liquidationProduct = new LiquidationProduct(name, price);
+            return liquidationProduct.getProduct();
+        }
+        return new Product(name, price);
+    }
+}
