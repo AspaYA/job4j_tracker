2f795077624f66efaf63e4800fd02aac9ae3c539
aspa <kreoll@gmail.com>
2020-10-22 12:52:42 +0300

    5.1. Тестирование. Подготовка данных. [#387541] https://job4j.ru/edu/task?action=task&taskId=0873958f751ce70201751ceec166046f&number=387541

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -14,8 +15,32 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         StartUI.add(input, tracker);
         Item created = tracker.findAll()[0];
-        System.out.println(created);
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"Fix PC"});
+        StartUI.add(input, tracker);
+        Input inputEdit = new StubInput(new String[]{"1", "edit Fix PC"});
+        StartUI.edit(inputEdit, tracker);
+
+        Item created = tracker.findAll()[0];
+        assertThat(created.getName(), is("edit Fix PC"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"Fix PC"});
+        StartUI.add(input, tracker);
+        Item created = tracker.findAll()[0];
+        assertThat(created.getName(), is("Fix PC"));
+
+        Input inputDel = new StubInput(new String[]{"1"});
+        StartUI.delete(inputDel, tracker);
+        assertThat(tracker.findAll().length, is(0));
     }
 }
